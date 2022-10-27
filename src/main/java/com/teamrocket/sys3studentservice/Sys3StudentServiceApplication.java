78ad230bbf99a2bf14513f30ab83b27c171f25e6
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2022-10-27 16:01:25 +0200

    initial commit

---
+++ src/main/java/com/teamrocket/sys3studentservice/Sys3StudentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teamrocket.sys3studentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sys3StudentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sys3StudentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamrocket/sys3studentservice/Sys3StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamrocket.sys3studentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sys3StudentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
