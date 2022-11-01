3ded4ff905957264a4fd8aee7181b588b7f28924
Mathias Parking <cph-mp525@cphbusiness.dk>
2022-11-01 13:59:36 +0100

    return recommendations

--- service/src/main/java/com/teamrocket/sys3studentservice/application/grcp/BookPurchaseServiceImpl.java
+++ service/src/main/java/com/teamrocket/sys3studentservice/application/grcp/BookPurchaseServiceImpl.java
@@ -3,6 +3,8 @@ package com.teamrocket.sys3studentservice.application.grcp;
 import com.teamrocket.BookPurchaseServiceGrpc.BookPurchaseServiceImplBase;
 import com.teamrocket.BookToBuy;
 import com.teamrocket.BoughtBook;
+import com.teamrocket.BoughtBookReply;
+import com.teamrocket.Recommendation;
 import com.teamrocket.sys3studentservice.dto.BookDto;
 import com.teamrocket.sys3studentservice.dto.IDDto;
 import com.teamrocket.sys3studentservice.service.CourseService;
@@ -12,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -23,15 +26,23 @@ public class BookPurchaseServiceImpl extends BookPurchaseServiceImplBase {
     private final KafkaTemplate<String, IDDto<Long>> kafkaTemplate;
 
     @Override
-    public void purchaseBook(BookToBuy request, StreamObserver<BoughtBook> responseObserver) {
+    public void purchaseBook(BookToBuy request, StreamObserver<BoughtBookReply> responseObserver) {
         BookDto bookDto = BookDto.builder()
             .id(request.getBookId())
             .price(request.getPrice())
             .build();
         IDDto<Long> bookBought = studentService.addBookToStudent(request.getStudentId(), bookDto);
         List<IDDto<Long>> recommendations = courseService.getRecommendations(bookBought.getId());
-        // TODO: returner recommendations med gRPC
+        List<Recommendation> recommendationsReply = new ArrayList<>();
+        for (IDDto dto: recommendations
+             ) {
+            recommendationsReply.add(Recommendation.newBuilder().setId((Long) dto.getId()).build());
+        }
         kafkaTemplate.send("bookBought", bookBought);
-        responseObserver.onNext(BoughtBook.newBuilder().setId(bookBought.getId()).build());
+        BoughtBookReply reply = BoughtBookReply.newBuilder()
+                .setBoughtBookId(bookBought.getId())
+                .addAllRecommendations(recommendationsReply)
+                .build();
+        responseObserver.onNext(reply);
     }
 }
